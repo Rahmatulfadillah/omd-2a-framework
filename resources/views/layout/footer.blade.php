35f8ba0dc31ff7e033f67697d0786eb960d0f866
Rahmatulfadillah <fadillahrahma885@gmail.com>
2026-04-09 14:41:54 +0700

    first commit

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+                <div class="footer-left">
+                    Copyright &copy; <span id="years"></span>
+                    <div class="bullet"></div> Rahmatul fadillah
+                </div>
+                <div class="footer-right">
+                    D3 Sistem Informasi
+                </div>
+            </footer>
+        </div>
+    </div>
